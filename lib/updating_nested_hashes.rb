f89b33d17142afddab9966a564e8af666f37080d
Akiko Green <akikogreen08@gmail.com>
2020-04-28 02:02:50 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -68,7 +68,6 @@ def add_characters
   apothecary = {name: "Apothecary"}
   epic_tragedy[:additional_characters][0] = { name: "Prince Escalus" }
 epic_tragedy[:additional_characters][1] = { name: "Apothecary" }
-  epic_tragedy[:additional_characters]
-
 end
+epic_tragedy
 end
